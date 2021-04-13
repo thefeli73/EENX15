8585e34c7320235c3d3222c31158d0aa62396cfa
Thefeli73 <thefeli73@hotmail.com>
2021-04-13 23:43:54 +0200

    K matrix

--- EENX15_LQR/LQR.ino
+++ EENX15_LQR/LQR.ino
@@ -1,5 +1,9 @@
 /** Control Law*/
 const float K[statesNumber] = {0.55192, 8.9867, 0.194, 0.39237};
+
+//K from our calculation
+//const float K[statesNumber] = {-31.6228, -21.2864, 80.7894, 13.4246};
+
 /** Correction factor */
 const float scaleConst = 2.5;
 
