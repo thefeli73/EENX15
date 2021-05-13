baa64b6240151d0c51e8850f258ce0917670ff30
Thefeli73 <thefeli73@hotmail.com>
2021-05-13 11:38:40 +0200

    clean garbage

--- EENX15_LQR/LQR.ino
+++ EENX15_LQR/LQR.ino
@@ -1,40 +1,17 @@
-//LQR-stuff
-//#include "Arduino_skal.h"
-
-// | ///////////////////////////////////
-// | //Row 24-52 in Arduino_skal_data.cpp
-// v ///////////////////////////////////
-
-  // Expression: A
-  //  Referenced by: '<Root>/Gain4'
+//LQR.ino
 
 const double matrix_A [16] = { 0.0, 0.0, 0.0, 0.0, 
   1.0, -0.20780947085442231, 0.0, -0.52810302415000854,
   0.0, 13.239785742831822, 0.0, 58.601480177829842, 
   0.0, 0.0, 1.0, 0.0 };
 
-//const double matrix_A [16] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-
-  // Expression: C
-  //  Referenced by: '<Root>/Gain6'
-
 const double matrix_C [8] = { 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 };
-//const double matrix_C [8] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
-
-  // Expression: L
-  //  Referenced by: '<Root>/Gain2'
 
 const double matrix_L [8] = { 56.7847, 799.5294, -1.4914, -57.4160, 
   -1.0363, -16.1071, 57.0075, 870.8172 };
 const double matrix_L_old [8] = { 116.63033952875418, 3387.8673967111704, -1.4473912197449676,
     -115.34372132703447, -1.0534041975488044, -48.223441605702455,
     117.16185100039935, 3490.0480780568214 };
-//const double matrix_L [8] = { 116.63033952875418, 338.78673967111704, -1.4473912197449676,
-//    -115.34372132703447, -1.0534041975488044, -48.223441605702455,
-//    117.16185100039935, 34.900480780568214 };
-
-  // Expression: B
-  //  Referenced by: '<Root>/Gain3'
 
 const double matrix_B [4] = { 0.0, 2.078094708544223, 0.0, 5.2810302415000852 };
 
@@ -42,25 +19,12 @@ const double matrix_B [4] = { 0.0, 2.078094708544223, 0.0, 5.2810302415000852 };
 const double matrix_K_old [4] = {-31.622776601683942,   -21.286439360075747,   80.789376267003959,    13.42463576551093};
 const double matrix_K [4] = {-0.0316,   -0.3938,   22.9455,    3.0629};
 
-// | ///////////////////////////////////
-// | //Row 261-264 in Arduino_skal.cpp
-// v ///////////////////////////////////
 double Integrator1_CSTATE [4] = {0.0, 0.0, 0.0, 0.0};
 double Sum3[4];
 double Sum4[4];
 
-// | ///////////////////////////////////
-// | //Row 123-124 in Arduino_skal.cpp
-// v ///////////////////////////////////
-
 double tmp[2];
 double rtb_Saturation = 0.0;
-
-// | ///////////////////////////////////
-// | //Row 140-143 in Arduino_skal.cpp
-// v ///////////////////////////////////
-
-// Denna funktion bör anropas när styrka + riktning till motorer ska bestämmas.
 double saturatedSignalToMotors(){
   rtb_Saturation = ((matrix_K[0] * Integrator1_CSTATE[0] +
              matrix_K[1] * Integrator1_CSTATE[1]) +
@@ -77,9 +41,6 @@ double saturatedSignalToMotors(){
   Serial.print("Saturation  = "); Serial.println(rtb_Saturation);
   return rtb_Saturation;
 }
-// | ///////////////////////////////////
-// | //Row 165-188 in Arduino_skal.cpp
-// v ///////////////////////////////////
 double inputToControlSystem(float position_m, float angle_r){
   float posAndAng[] = {position_m, angle_r};
   for (int i = 0; i < 2; i++) {
@@ -89,15 +50,7 @@ double inputToControlSystem(float position_m, float angle_r){
       Integrator1_CSTATE[2]) + matrix_C[i + 6] *
       Integrator1_CSTATE[3]);
   }
-
-  // End of Sum: '<Root>/Sum2'
   for (int i = 0; i < 4; i++) {
-    // Sum: '<Root>/Sum4' incorporates:
-    //   Gain: '<Root>/Gain2'
-    //   Gain: '<Root>/Gain3'
-    //   Gain: '<Root>/Gain4'
-    //   Integrator: '<Root>/Integrator1'
-    //   Sum: '<Root>/Sum3'
   
   
     Sum3[i] = ((matrix_L[i + 4] * tmp[1] + matrix_L[i]
@@ -122,29 +75,8 @@ double inputToControlSystem(float position_m, float angle_r){
   Arduino_skal_derivatives();
   return saturatedSignalToMotors();
 }
-
-// | ///////////////////////////////////
-// | //Row 215-225 in Arduino_skal.cpp
-// v ///////////////////////////////////
-
 void Arduino_skal_derivatives()
 {
-  /*
-  XDot *_rtXdot;
-  _rtXdot = ((XDot *) (&rtM)->derivs);
-  // Derivatives for Integrator: '<Root>/Integrator1'
-  _rtXdot->Integrator1_CSTATE[0] = Sum4[0];
-  _rtXdot->Integrator1_CSTATE[1] = Sum4[1];
-  _rtXdot->Integrator1_CSTATE[2] = Sum4[2];
-  _rtXdot->Integrator1_CSTATE[3] = Sum4[3];
-
-  
-  Integrator1_CSTATE[0] = Sum4[0] * (fastTimer/1000.0);
-  Integrator1_CSTATE[1] = Sum4[1] * (fastTimer/1000.0);
-  Integrator1_CSTATE[2] = Sum4[2] * (fastTimer/1000.0);
-  Integrator1_CSTATE[3] = Sum4[3] * (fastTimer/1000.0);
-*/
-  // Derivatives for Integrator: '<Root>/Integrator1'
   for (int i = 0; i < 4; i++) {
     Integrator1_CSTATE[i] = Sum4[i] * fastTimer/1000.0;
     Serial.print("Integrator: "); Serial.println(Integrator1_CSTATE[i]);
--- EENX15_LQR/lqr_fullstate.ino
+++ EENX15_LQR/lqr_fullstate.ino
@@ -1,13 +1,5 @@
+//lqr_fullstate.ino
 float lqr_fullstate(float position_m, float speed_ms, float angle_r, float angle_speed_rs){
-//  const float matrix_K [4] = {-0.0316,   -0.3938,   22.9455,    3.0629};
-//  const float matrix_K [4] = {-1.0000,   -1.8855,   26.6999,    3.8592};
-//  const float matrix_K [4] = {-1.0000,   -1.9058,   27.0579,    3.8886};
-//  const float matrix_K [4] = {-0.3162,   -1.0381,   24.6060,    3.4143};
-//  const float matrix_K [4] = {-0.1054,   -0.6273,   23.5822,    3.1936};
-//  const float matrix_K [4] = {-0.1054,   -0.5273,   23.5546,    3.1876};
-//  const float matrix_K [4] = {-0.1054,   -0.5273,   23.5546,    3.1876};
-//  const float matrix_K [4] = {-0.7071,   -1.5720,   26.0726,    3.7050};
-//  const float matrix_K [4] = {-0.7071,   -1.5980,   26.6081,    4.3220};
   const float matrix_K [4] = {-0.7071,   -1.7751,   34.5368,    4.8793};
   
   float result;
@@ -15,24 +7,10 @@ float lqr_fullstate(float position_m, float speed_ms, float angle_r, float angle
            matrix_K[1] * speed_ms +
            matrix_K[2] * angle_r +
            matrix_K[3] * angle_speed_rs;
-  /*
-  if (result > 0.29) {
-    result = 0.29;
-  } else if (result < -0.29) {
-    result = -0.29;
-  } else {
-    result = result;
-  }*/
   Serial.print("K calculation (force): "); Serial.println(result); 
   return result;
 }
 float calc_speed(float input) {
-  /*
-  float a = -2971;
-  float b = -0.9929;
-  float c = 90.75;
-  float radps = a * pow(speed, b) + c; ////// the response graph
-  */
   float scale = 1.5;
   input = abs(input)*0.30796; // scale down to rad/s (78,53/255)
   Serial.print("input: "); Serial.println(input); 
